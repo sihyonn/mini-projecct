fdb7aacad1f7036e46a3d0b143b5f644d33c86e6
sihyonn <sihyeon982@naver.com>
2023-12-08 17:04:22 +0900

    feat: 카카오 지도 api 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 function App() {
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      '//dapi.kakao.com/v2/maps/sdk.js?appkey=f93d908946e0055b641a4c731a2d3110&autoload=false';
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        if (mapRef.current) {
+          var options = {
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3,
+          };
+
+          var map = new window.kakao.maps.Map(mapRef.current, options);
+        }
+      });
+    };
+    return () => script.remove();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div ref={mapRef} style={{ width: 300, height: 300 }}></div>
     </div>
   );
 }
